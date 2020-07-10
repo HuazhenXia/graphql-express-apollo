eb1614f442996a049b5489e3f2d1c5d03c3bdbdf
Huazhen Xia <imxhz@foxmail.com>
2020-07-10 04:33:47 -0700

    add 2 APIs about list

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use('/graphql', graphqlHTTP({
   schema,
-  // graphiql: true
+  graphiql: true
 }));
 
 app.listen(4000, () => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -4,22 +4,58 @@ const _ = require('lodash');
 const { GraphQLObjectType,
   GraphQLString,
   GraphQLID,
-  GraphQLSchema } = require('graphql');
+  GraphQLInt,
+  GraphQLList,
+  GraphQLSchema
+} = require('graphql');
+
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id })
+      }
+    }
+  }),
+});
 
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    authorId: { type:GraphQLID },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        console.log(_.find(authors, { id: parent.authorId }))
+        return _.find(authors, { id: parent.authorId })
+      }
+    }
   })
-})
+});
 
+// A book has A author. A Author has many books
 const books = [
-  { name: "Alogrithm", genre: "CS", id: "1" },
-  { name: "Network", genre: "CS", id: "2" },
-  { name: "World History", genre: "History", id: "3" }
+  { name: "Alogrithm", genre: "CS", id: "1", authorId: '1' },
+  { name: "Network", genre: "CS", id: "2", authorId: '2' },
+  { name: "World History", genre: "History", id: "3", authorId: '3' },
+  { name: "Human History", genre: "History", id: "4", authorId: '3' },
+  { name: "Bird History", genre: "History", id: "5", authorId: '3' }
 ]
+
+const authors = [
+  { name: "Ben", age: 27, id: "1" },
+  { name: "Lily", age: 30, id: "2" },
+  { name: "James", age: 21, id: "3" },
+];
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
@@ -28,9 +64,27 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        // get data
-        console.log(args)
+        // get data logic
         return _.find(books, { id: args.id })
+      }
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id })
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve() {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve() {
+        return authors;
       }
     }
   }
